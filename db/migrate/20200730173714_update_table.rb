62ee6438a1a4e9e5492f495797246079c59938d9
MatejHor <32205201+MatejHor@users.noreply.github.com>
2020-07-30 19:45:44 +0200

    Update migrations

--- db/migrate/20200730173714_update_table.rb
+++ db/migrate/20200730173714_update_table.rb
@@ -1,9 +1,9 @@
 class UpdateTable < ActiveRecord::Migration[6.0]
   def change
-    remove_column :exercises, :series
-    remove_column :exercises, :weight
+    remove_column :exercises, 'series'
+    remove_column :exercises, 'weigh'
     add_column :exercises, 'time', :string, null: true
-    add_column :exercises, 'exercises', :json, default: [], null: true
+    add_column :exercises, :exercises, :jsonb, null: false, default: []
     add_column :exercises, 'distance', :string, null: true
   end
 end
